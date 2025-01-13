b90faa8c19ed7d507f07902f2066ad723c32a925
Pratik Nikam <nikampratik2989@gmail.com>
2025-01-13 18:54:28 +0530

    Initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ const Contacts = () => {
     return (
         <section id="contact">
             <div className="contact__container">
-                <a href="https://www.linkedin.com/in/deepak-gupta-68bb2321a" target="_blank"><AiFillLinkedin /></a>
-                <a href="https://github.com/deepakrvg" target="_blank"><AiOutlineGithub /></a>
-                <a href="mailto:guptadeepak0908@gmail.com"><CgMail /></a>
+                <a href="https://www.linkedin.com/in/nikam-p-86576a239/" target="_blank"><AiFillLinkedin /></a>
+                <a href="https://github.com/nikampratik9096" target="_blank"><AiOutlineGithub /></a>
+                <a href="mailto:nikampratik2989@gmail.com"><CgMail /></a>
             </div>
         </section>
     )
