28e7ee4a48d79a1641d35173396dac7fb374c3d5
Pratik Rajendra Nikam <161759614+nikampratik9096@users.noreply.github.com>
2025-01-20 14:57:02 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                     <h2>About Me</h2>
                     <p>Hi I am Pratik Nikam.<br />I am Full Stack Developer and Competitive Programmer located in India.</p>
                     <p>I am 3* star coder on GeeksforGeeks and 1700 + rated on Leetcode . Its fun for me taking part in Competitive Programming. I am curious to know about how things work around.</p>
-                    <p>I passionate about programming and have a great interest in Full Stack Development. I am also fascinated about AI/ML.</p>                    
+                    <p>I passionate about programming and have a great interest in Full Stack Development.I am also fascinated about AI/ML.</p>                    
                 </div>
             </div>
         </section>
